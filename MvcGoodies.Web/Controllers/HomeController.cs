f5846ed30267452827fd73c9ff1a03b1b5f6055e
mhidinger <mhidinger@mhidinger1.clarityinternal.net>
2012-01-09 15:01:53 -0600

    Initial checkin

---
+++ MvcGoodies.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using MvcGoodies.Web.Models;
+
+namespace MvcGoodies.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var product = new ProductInput();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            var product = new ProductInput();
+            if(!TryUpdateModel(product, form))
+            {
+                return View(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
