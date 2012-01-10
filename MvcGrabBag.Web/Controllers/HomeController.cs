bb6e39c5f131d24b0ef5dcbbfea1176e09b22cfd
Matt Hidinger <1432195+matthidinger@users.noreply.github.com>
2012-01-10 09:20:32 -0600

    - Project rename

---
+++ MvcGrabBag.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MvcGrabBag.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
