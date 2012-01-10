cd7cec30fe6db0a386518e8eaa55e9efd689f4bf
Matt Hidinger <1432195+matthidinger@users.noreply.github.com>
2012-01-10 09:20:46 -0600

    - Cleanup

---
+++ MvcGrabBag.Web/Controllers/HtmlFormHelperController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using MvcGrabBag.Web.Models;
+
+namespace MvcGrabBag.Web.Controllers
+{
+    public class HtmlFormHelperController : Controller
+    {
+        [HttpGet]
+        public ActionResult NewProduct()
+        {
+            var product = new ProductInput();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult NewProduct(FormCollection form)
+        {
+            var product = new ProductInput();
+            if(!TryUpdateModel(product, form))
+            {
+                return View(product);
+            }
+
+            // TODO: Would Save Product here
+
+            return RedirectToAction("NewProduct");
+        }
+
+    }
+}
